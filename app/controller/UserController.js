8a3582cf25419295fe7667a8267ee7fcfd3252d2
anan <hoakusa@gmail.com>
2015-10-08 12:15:06 +0300

    switch to id + fix update syntax

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -1,13 +1,13 @@
-var pwdMgr = require('../middleware/password');
-var config = require('../config');
-var authentication = require('../middleware/authentication');
-var jwt    = require('jsonwebtoken');
-var mongojs = require('mongojs');
+var pwdMgr          = require('../middleware/password');
+var config          = require('../config');
+var authentication  = require('../middleware/authentication');
+var jwt             = require('jsonwebtoken');
+var mongojs         = require('mongojs');
  
 module.exports = function(server, db) {
 
     // readAll
-    server.get('/api/user/all', authentication, function (req, res, next) {
+    server.get('/api/user/all', function (req, res, next) {
         db.users.find(function (err, dbUser) {
             res.send(200, dbUser);
         });
@@ -57,50 +57,44 @@ module.exports = function(server, db) {
     server.post('/api/user/', function (req, res, next) {
         var user = req.params;
 
-        if (user.email.length === 0 || user.password.length === 0 || user.username.length === 0) {
+        if (user.email.length === 0 || user.password.length === 0) {
             res.send(403, { message: 'Username or password hasn\'t been input.' });
             return next();
         }
 
-        db.users.findOne({username: user.username}, function (err, dbUser) {
+        db.users.findOne({email: user.email}, function (err, dbUser) {
             if (err) throw err;
 
             if (!!dbUser) {
-                res.send(400, { message: 'A user with this username already exists' });
+                res.send(400, { message: 'A user with this email already exists' });
                 return next();
             }
 
-            db.users.findOne({email: user.email}, function (err, dbUser1) {
-                if (err) throw err;
+            pwdMgr.cryptPassword(user.password, function (err, hash) {
+                user.password = hash;
+                console.log("n", hash);
 
-                if (!!dbUser1) {
-                    res.send(400, { message: 'A user with this email already exists' });
-                    return next();
-                }
+                // set default value
+                user.username = "Instagram User";
+                user.avatar = "";
 
-                pwdMgr.cryptPassword(user.password, function (err, hash) {
-                    user.password = hash;
-                    console.log("n", hash);
-
-                    db.users.insert(user, function (err, dbUser2) {
-                        if (err) { // duplicate key error
-                            if (err.code == 11000) /* http://www.mongodb.org/about/contributors/error-codes/*/ {
-                                res.send(400, { error: err, message: "A user with this email already exists" });
-                            }
-                        } else {
-                            res.send(200, { message: 'Registered successfully!' });
+                db.users.insert(user, function (err, dbUser2) {
+                    if (err) { // duplicate key error
+                        if (err.code == 11000) /* http://www.mongodb.org/about/contributors/error-codes/*/ {
+                            res.send(400, { error: err, message: "A user with this email already exists" });
                         }
-                    });
+                    } else {
+                        res.send(200, { message: 'Registered successfully!' });
+                    }
                 });
             });
-        });
-        
+        });        
         return next();
     });
 
     //read other profile
-    server.get('/api/user/:username', authentication, function (req, res, next) {
-        db.users.findOne({ username: req.params.username }, function (err, dbUser) {
+    server.get('/api/user/:id', authentication, function (req, res, next) {
+        db.users.findOne({ _id: mongojs.ObjectId(req.params.id) }, function (err, dbUser) {
             if (err) throw err;
 
             if (!dbUser) { // user doesnt exist
@@ -134,11 +128,6 @@ module.exports = function(server, db) {
     server.put('/api/user', authentication, function (req, res, next) {
         var editUser = req.params;
 
-        if (editUser.username === 0) {
-            res.send(403, { message: 'Username is required!' });
-            return next();
-        }
-
         db.users.findOne({ _id: mongojs.ObjectId(req.reqUser._id) }, function (err, dbUser) {
             if (err) throw err;
 
@@ -147,25 +136,18 @@ module.exports = function(server, db) {
                 return next();
             }
 
+            var saveUser = dbUser;
+
             if (!!editUser.username && editUser.username !== dbUser.username) {
-
-                db.users.findOne({username: user.username}, function (err, dbUser2) {
-                    if (err) throw err;
-
-                    if (!!dbUser2) {
-                        res.send(400, { message: 'A user with this username already exists' });
-                        return next();
-                    }
-
-                    dbUser.username = editUser.username;
-                });
+                saveUser.username = editUser.username;
+                console.log(saveUser.username);
             }
 
             if (!!editUser.newPassword && editUser.newPassword !== "") {
                 pwdMgr.comparePassword(editUser.oldPassword, dbUser.password, function (err, isPasswordMatch) {
  
                     if (isPasswordMatch) {                    
-                        dbUser.password = editUser.newPassword;
+                        saveUser.password = editUser.newPassword;
                         res.send(200, { message: "Password changed!" });
                     } else {
                         res.send(403, { message: "Authentication failed. Wrong password." });
@@ -176,10 +158,10 @@ module.exports = function(server, db) {
             }
 
             if (!!editUser.avatar) {
-                dbUser.avatar = editUser.avatar;
+                saveUser.avatar = editUser.avatar;
             }
 
-            db.users.update({ _id: mongojs.ObjectId(req.reqUser._id) }, function (err, dbUser) {
+            db.users.update({ _id: mongojs.ObjectId(req.reqUser._id) }, saveUser, function (err, dbUser) {
                 if (err) throw err;
                 res.send(200, { message: 'Profile updated!' });
             });
